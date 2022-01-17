549e8cb6622a938be8c1be784340f2cd77cebd92
EthanJung1999 <81884006+EthanJung1999@users.noreply.github.com>
2022-01-17 14:59:46 -0800

    Added basic structure of Audio System

---
+++ Spirit Kin/Assets/Scripts/Audio_system.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_system : MonoBehaviour
+{
+    public AudioClip slash;
+    void Update()
+    {
+    }
+    
+    
+}
