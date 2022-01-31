85d15ec0a535d1960c7ba56d4d9c874e5fb59b25
Adrian <adrianlance86@gmail.com>
2022-01-31 13:53:13 -0800

    Made a successful combo system for the player

--- Spirit Kin/Assets/Scripts/Player_Battle_Controller.cs
+++ Spirit Kin/Assets/Scripts/Player_Battle_Controller.cs
@@ -7,16 +7,13 @@ public class Player_Battle_Controller : MonoBehaviour
     public float DodgeTime = 0;
     private float DodgeCoolDown = 0f;
     public float TotalDodgeTime = 0.2f;
-    public float LungeTime = 0;
-    private float TotalLungeTime = 0.16f;
-    public float LungeCoolDown = 0f;
     private float distanceFromTarget;
     public Vector3 DodgeDirection;
     public GameObject Sword;
-    public float AttackCoolDownTime = 1f;
     public int numOfClicks = 0;
     private Animator animator;
     private float ComboTimeDelay;
+    private float TotalAnimationTime;
 
     private void Start() {
         Sword.GetComponent<Collider>().enabled = false;
@@ -38,13 +35,20 @@ public class Player_Battle_Controller : MonoBehaviour
             distanceFromTarget = 100f;
         }
 
-        if(ComboTimeDelay < 0.9){
+        //Handels animating combos
+        if(FindObjectOfType<Lock_Target>().Target == null){
+            TotalAnimationTime = animator.GetCurrentAnimatorStateInfo(0).length;
+        }else{
+            TotalAnimationTime = animator.GetCurrentAnimatorStateInfo(1).length;
+        }
+        if(ComboTimeDelay < TotalAnimationTime){
             ComboTimeDelay += Time.deltaTime;
         }else{
             numOfClicks = 0;
+            ComboTimeDelay = 0;
             animator.SetInteger("attackTicks", numOfClicks);
         }
-        if(ComboTimeDelay >= 0.7f )
+        if(ComboTimeDelay >= TotalAnimationTime - (TotalAnimationTime/3) || numOfClicks == 0)
             Attack();
     }
     private void Dodge(){
@@ -59,6 +63,9 @@ public class Player_Battle_Controller : MonoBehaviour
             ComboTimeDelay = 0f;
             Sword.GetComponent<Collider>().enabled = true;
             numOfClicks++;
+            if(numOfClicks > 3){
+                numOfClicks = 1;
+            }
             animator.SetInteger("attackTicks", numOfClicks);
         }
     }
