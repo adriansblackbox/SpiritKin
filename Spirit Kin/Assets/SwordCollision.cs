bee403f7f36a437728001c86d9885e7943926682
Adrian <adrianlance86@gmail.com>
2022-02-15 15:47:12 -0800

    sword working now with raycast

--- Spirit Kin/Assets/SwordCollision.cs
+++ Spirit Kin/Assets/SwordCollision.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class SwordCollision : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.CompareTag("Enemy")){
-            Debug.Log("HIT!");
-            //other.GetComponent<CharacterStats>().TakeDamage(damage.GetValue());
+    public Transform BladeRayOrigin;
+    public float BladeLength = 10f;
+    public LayerMask layerMask;
+    private void Update() {
+        RaycastHit hit;
+        if (Physics.Raycast(BladeRayOrigin.position, BladeRayOrigin.TransformDirection(Vector3.left), out hit, BladeLength, layerMask))
+        {
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.DrawRay(BladeRayOrigin.position, BladeRayOrigin.TransformDirection(Vector3.left) * BladeLength, Color.red);
+            Debug.Log("Did Hit");
+            hit.transform.gameObject.GetComponent<CharacterStats>().TakeDamage(20);
+        }
+        else
+        {
+            Debug.DrawRay(BladeRayOrigin.position, BladeRayOrigin.TransformDirection(Vector3.left) * BladeLength, Color.yellow);
         }
     }
 }
