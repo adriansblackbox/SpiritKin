9684fc9011a1352a0f76dfb5475a304142dc3346
Adrian <adrianlance86@gmail.com>
2022-03-01 00:12:40 -0800

    temp VFX progress, and equipment cart in

---
+++ Spirit Kin/Assets/StatVFX.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatVFX : MonoBehaviour
+{
+    public Image health1, health2, armor1, armor2, speed1, speed2, damage1, damage2;
+    public Sprite notch, upArrow, downArrow;
+
+    private void Update() {
+        
+    }
+}
