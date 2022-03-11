a885cf82c2eee41ebb4c18bbafc9869f3dcfeb9f
Adrian <adrianlance86@gmail.com>
2022-03-10 23:46:47 -0800

    starting special attack progress

---
+++ Spirit Kin/Assets/AOE_Special.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AOE_Special : MonoBehaviour
+{
+    public GameObject Collider;
+    private void Start() {
+        Collider.SetActive(false);
+    }
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Mouse1)){
+            Collider.SetActive(true);
+        }
+        if(Input.GetKeyUp(KeyCode.Mouse1)){
+            Collider.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Enemy")){
+            other.gameObject.GetComponent<CharacterStats>().TakeDamage(50f);
+        }
+    }
+}
