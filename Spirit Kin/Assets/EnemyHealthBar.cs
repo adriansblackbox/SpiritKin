6187eed1d7e921506a6760234b1365c974c5c3cb
DLBrab <82184836+DLBrab@users.noreply.github.com>
2022-04-21 19:37:07 -0700

    Health bar + Gitignore update
    Health bars now exist and show when enemies are damaged.
    Known error on both death and start that the display is incorrect.
    Update to gitignore to just ignore all .meta files. Probably again. I'm getting slapped by these meta files basically

--- Spirit Kin/Assets/EnemyHealthBar.cs
+++ Spirit Kin/Assets/EnemyHealthBar.cs
@@ -6,18 +6,43 @@ public class EnemyHealthBar : MonoBehaviour
 {
     [SerializeField]
     private CharacterStats enemy_Stats;
-    public GameObject healthBar;
-    public GameObject camera;
+
+    [SerializeField]
+    private float updateSpeed;
+
+    private GameObject healthBar;
+    private float previousPCT;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         gameObject.SetActive(false);
+        previousPCT = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthBar.transform.localScale = new Vector3 (7 * (enemy_Stats.currentHealth / enemy_Stats.maxHealth), 0.3f, 1f);
+        
+    }
+
+    void LateUpdate () 
+    {
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, 180, 0);
+    }
+
+    public IEnumerator takeDamageUI(float newPCT) 
+    {
+        float elapsed = 0f;
+
+        while (elapsed < updateSpeed) {
+            elapsed += Time.deltaTime;
+            healthBar.transform.localScale = new Vector3 (Mathf.Lerp(40 * previousPCT, 40 * newPCT, elapsed / updateSpeed), 0.3f, 1f);
+            yield return null;
+        }
+
+        previousPCT = newPCT;
     }
 }
