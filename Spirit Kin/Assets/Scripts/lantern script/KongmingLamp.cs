b1151a1453b1aa27b3bd345b8c71dcc2fe1a66b4
Yukai Liu <yliu637@ucsc.edu>
2022-05-16 20:07:59 -0700

    Lantern improved

--- Spirit Kin/Assets/Scripts/lantern script/KongmingLamp.cs
+++ Spirit Kin/Assets/Scripts/lantern script/KongmingLamp.cs
@@ -2,28 +2,27 @@ using UnityEngine;
 
 public class KongmingLamp : MonoBehaviour
 {
-    private Transform trans;
+    
     private Vector3 speed;
-
 
     void Start()
     {
-        trans = transform;
         // 随机一个升空速度
         speed = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(1, 3), Random.Range(-0.5f, 0.5f));
+
     }
 
 
     void Update()
     {
-        // 沿着速度方向升空
-        trans.position += speed * Time.deltaTime;
+        // Ascending speed
+        transform.position += speed * Time.deltaTime;
 
-        // 高度达到n，自我销毁
-        if (trans.position.y > 1000)
+        // destroyed gameObject when reach to n
+        if (transform.position.y > 102)
         {
+            //lanternObj.SetActive(false);
             Destroy(gameObject);
         }
-
     }
 }
